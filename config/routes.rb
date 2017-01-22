5494ca691cf4bac2ff8f1e0911f890f8214291d6
Amber <amber@amberwilkie.com>
2017-01-22 10:21:30 +0100

    Fix up root routing

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   end
 
   # get '/', controller: :events, action: :index
-  root to: '/events', controller: :events, action: :index
+  root to: '/events', controller: 'events/events', action: :index
 end
